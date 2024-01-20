55a307e3e56a8c714beea18e41462a9ad760c0c2
Emeka Okoli <emekaokoli00@gmail.com>
2024-01-20 09:25:00 +0100

    routes index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,17 @@
 import { Router } from 'express';
 import { router as appointmentRouter } from './appointment';
-import { router as patientRouter } from './patients';
+import { router as patientRouter } from './patients.routes';
+import { router as providerRouter } from './providers.routes';
 
 import { router as authRouter } from './auth';
 
 const router = Router();
 
 router.use('/patients', patientRouter);
-router.use('/appointment', appointmentRouter);
-router.use('/providers', appointmentRouter);
-router.use('/', authRouter);
+router.use('/providers', providerRouter);
+router.use('/appointments', appointmentRouter);
+router.use('/login', authRouter);
+router.use('/register', authRouter);
 
 export { router };
+
