c89c8c6a588ff9d620254570b57dca655305c100
Emeka Okoli <emekaokoli00@gmail.com>
2024-01-25 00:11:04 +0100

    health check

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,14 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
 import { router as appointmentRouter } from './appointment.routes';
 import { router as authRouter } from './auth.routes';
 import { router as providerRouter } from './providers.routes';
 
 const router = Router();
-
-// router.use('/patients', patientRouter);
+router.get('/healthcheck', (_: Request, res: Response) => {
+  res.sendStatus(200);
+});
 router.use('/providers', providerRouter);
 router.use('/appointments', appointmentRouter);
 router.use('/auth', authRouter);
 
 export { router };
-
