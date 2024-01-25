fe6242a7ca923a14c92b678dd02c885c31d4529d
Emeka Okoli <emekaokoli00@gmail.com>
2024-01-25 01:09:09 +0100

    knex config errors fixed

--- src/db/knex.ts
+++ src/db/knex.ts
@@ -1,8 +1,9 @@
-import knex from 'knex';
-import config from '../../knexfile.js';
+// knex.ts
 
+import knex from 'knex';
+import Config from '../../knexfile';
 const environment = 'development';
-const knexConfig = config[environment];
+const knexConfig = Config[environment];
 const knexInstance = knex(knexConfig);
 
 export default knexInstance;
